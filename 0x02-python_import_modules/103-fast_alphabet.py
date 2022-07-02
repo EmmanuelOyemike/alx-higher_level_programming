41b41d352aeeaf23b0719232bd7fdabfb41103d2
Iscah <emmaoyemike@gmail.com>
2022-07-02 01:32:42 -0700

    delete

--- 0x02-python_import_modules/103-fast_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# Author: Emmanuel Oyemike
-
-import string
-print(string.ascii_uppercase)
--- 0x02-python_import_modules/variable_load_5.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# Author: Emmanuel Oyemike
-
-a = 98
-"""Simple variable
-"""
