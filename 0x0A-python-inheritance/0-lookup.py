cfc47b62958df224fb8053ead4e9264ca5df90ae
EmmanuelOyemike <emmaoyemike@gmail.com>
2022-08-01 23:02:30 +0000

    python inheritance files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Emmanuel Oyemike
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))#!/usr/bin/python3
+# 0-lookup.py
+# Bright
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
